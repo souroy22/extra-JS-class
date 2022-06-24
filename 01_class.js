1ff2bf7ee5827d800ce53c575fdbdcbfa8d420fd
Sourav Roy <sroy3166@gmail.com>
2022-06-24 23:27:18 +0530

    added last code

---
+++ 01_class.js
@@ -0,0 +1,87 @@
+
+var a = 10 // function scope variable
+
+let b = 11; // block scope variables
+console.log(b);
+abc();
+function abc () {
+     
+}
+
+
+// scope
+// if(true){
+//      let c = 10;
+//      console.log("hello");
+//      if(true){
+//           console.log(c);
+//      }
+     
+// }
+
+// const car = {
+//      door: '',
+
+// };
+
+// abc()
+// function abc () {
+
+// }
+
+// // let a = 10;
+// // a = 11
+
+
+// if(a > b){
+//      c = "sskskk"
+// }else{
+//      // c = 13;
+// }
+// // console.log(c);
+
+// const arr = [1, "sjjsj", 28882, {a: "sksdkkd"}, 100];
+
+// for(var i=0;i<arr.length;i++){
+     
+// }
+
+// let i = 0;
+// while(i!=0){
+//      if(a < 10){
+//           i++;
+//      }
+//      console.log(i);
+// }
+
+// let type = "INCREMENT";
+
+// switch (type) {
+//      case 'INCREMENT':
+//           a++;
+//           break;
+//      case 'DECREMENT':
+//           a--;
+//           break;
+//      case 'MULTIPLY':
+//           a *= 2;
+//           break;
+//      default:
+//           console.log(a);
+// }
+
+// if(type === 'INCREMENT'){
+//      a++;
+// }else if(type === 'DECREMENT'){
+//      a--;
+// }else if(type === 'MULTIPLY'){
+//      a *= 2;
+// }else{
+//      console.log(a);
+// }
+
+// do{
+//      console.log(a);
+// }
+// while(i!=0)
+
---
+++ printStart.js
@@ -0,0 +1,10 @@
+// 1, 2, 3, 4, 5
+for(let i=1;i<6;i++){
+     // console.log(i);
+}
+
+// 2, 4, 8, 16, 32
+for(let i=2;i<=32;i = i*2){
+     console.log(i);
+}
+
